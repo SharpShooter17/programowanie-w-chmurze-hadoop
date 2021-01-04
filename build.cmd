ae2ba92cffc8ea0a2a8f07300876990ffeea3eb2
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2021-01-04 13:13:15 +0100

    curl

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
+curl https://archive.apache.org/dist/hadoop/core/hadoop-3.1.4/hadoop-3.1.4.tar.gz --output hadoop-3.1.4.tar.gz
 cp config/* master/config/ -r
 cp config/* worker/config/ -r
 cp hadoop-3.1.4.tar.gz master
